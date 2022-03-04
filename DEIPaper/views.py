8291a0ee2cdda161c99ea7cf27f324866a8630e9
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-03-04 12:25:49 +0000

    Added basic Paper model

--- DEIPaper/views.py
+++ DEIPaper/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(response):
-  return HttpResponse("Hello, world. You're at the DEIPaper index!")
+  return HttpResponse("<h1>Hello, world. You're at the DEIPaper index!</h1>")
--- ideas.md
+++ ideas.md
@@ -1,1 +1,3 @@
 - even though in "view all" mode we only have to show title and authors, it'd be nice to "expand" the section to show all the fields
+
+- be able to search all papers from a given author
