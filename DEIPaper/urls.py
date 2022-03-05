5239b39d5693343bbdd9bf8481093889f6e0a784
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-03-05 22:25:47 +0000

    HTML homepage done, missing connecting w/ API and styling

--- DEIPaper/urls.py
+++ DEIPaper/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
+  # home page redirections
+  path('home/', views.index, name='home'),
   path('', views.index, name='index'),
+  # general redirections
+  path('list-all-papers/', views.list_all_papers, name='list-all-papers'),
+  path('list-specific-paper/', views.list_specific_paper, name='list-specific-paper'),
+  path('create-paper/', views.create_paper, name='create-paper'),
+  path('edit-paper/', views.edit_paper, name='edit-paper'),
+  path('delete-paper/', views.delete_paper, name='delete-paper'),
 ]
--- DEIPaper/views.py
+++ DEIPaper/views.py
@@ -1,6 +1,56 @@
 from django.shortcuts import render
+import requests, os
+from .forms import ListAllPapersForm, ListSpecificPaperForm, CreatePaperForm, EditPaperForm, DeletePaperForm
 
 # Create your views here.
 
+PAPERS_ENDPOINT = os.getenv(
+  "PAPERS_ENDPOINT",
+  default="https://aduck.rnl.tecnico.ulisboa.pt/istpaper/papers"
+)
+
 def index(request):
-  return render(request, "DEIPaper/base.html", {}) # TODO - change this to index.html
+  forms = [ListAllPapersForm(request.GET), ListSpecificPaperForm(request.GET), CreatePaperForm(request.POST), EditPaperForm(request.POST), DeletePaperForm(request.POST)]
+  return render(request, "DEIPaper/index.html", {"forms":forms})
+
+def list_all_papers(request):
+  # limit = int(request.GET.get("limit", default=40))
+  # offset = int(request.GET.get("offset", default=1))
+  # print("limit:", limit)
+  # print("offset:", offset)
+  # answer = requests.get(PAPERS_ENDPOINT, params={'limit':limit, 'offset':offset})
+  # if not limit:
+  #   limit = answer.json()['total']
+  # if not offset:
+  #   offset = 1
+  # print("Starting to list papers...")
+  # print(answer.text)
+  # print("Done!")
+  if request.method == "GET":
+    print("Gotcha!")
+  form = ListAllPapersForm(request.GET)
+  return render(request, "DEIPaper/index.html")
+
+def list_specific_paper(request):
+  if request.method == "GET":
+    print("Gotcha!")
+  form = ListSpecificPaperForm(request.GET)
+  return render(request, "DEIPaper/index.html", {"forms":[form]})
+
+def create_paper(request):
+  if request.method == "POST":
+    print("Gotcha!")
+  form = CreatePaperForm(request.POST)
+  return render(request, "DEIPaper/index.html", {"forms":[form]})
+
+def edit_paper(request):
+  if request.method == "POST":
+    print("Gotcha!")
+  form = EditPaperForm(request.POST)
+  return render(request, "DEIPaper/index.html", {"forms":[form]})
+
+def delete_paper(request):
+  if request.method == "POST":
+    print("Gotcha!")
+  form = DeletePaperForm(request.POST)
+  return render(request, "DEIPaper/index.html", {"forms":[form]})
