b15e6f0edb6632ec93ef7efa51345d48aa814a0e
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-03-05 10:34:38 +0000

    Add base.html skeleton

--- DEIPaper/views.py
+++ DEIPaper/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
-def index(response):
-  return HttpResponse("<h1>Hello, world. You're at the DEIPaper index!</h1>")
+def index(request):
+  return render(request, "DEIPaper/base.html", {}) # TODO - change this to index.html
--- exbiidei/urls.py
+++ exbiidei/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('papers/', include('DEIPaper.urls')),
     path('', include('DEIPaper.urls')),
 ]
